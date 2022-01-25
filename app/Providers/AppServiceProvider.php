7c7f4e77cefc573df773b1baa78dd57658b45afc
RobertJDevOP <rcjimenez35@gmail.com>
2022-01-25 15:09:18 -0500

    feat: rate limiter are modified

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,15 +2,13 @@
 
 namespace App\Providers;
 
-use App\Repositories\Eloquent\ProductRepository;
-use App\Repositories\Interfaces\ProductRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->app->bind(ProductRepositoryInterface::class, ProductRepository::class);
+        //
     }
 
     public function boot(): void
